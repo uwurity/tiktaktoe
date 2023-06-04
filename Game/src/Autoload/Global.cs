af774199319135cc00b217fa38bc3fc3760e3725
Daniel Phan <danielphan.2003@gmail.com>
2023-06-05 01:28:08 +0700

    feat(autoload): add global access to scene manager instance

---
+++ Game/src/Autoload/Global.cs
@@ -0,0 +1,19 @@
+using Godot;
+
+namespace tiktaktoe.Autoload;
+
+public partial class Global : Node
+{
+	public Node SceneManager { get; private set; } = null!;
+
+	private const string SceneManagerPath = "/root/SceneManager";
+	
+	public override void _Ready()
+	{
+		SceneManager = GetNode(SceneManagerPath);
+	}
+
+	public override void _Process(double delta)
+	{
+	}
+}
