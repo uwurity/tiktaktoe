cb81e59d3188f8142ac231d8946316d3c534c338
Daniel Phan <danielphan.2003@gmail.com>
2023-06-05 02:25:02 +0700

    refactor(SceneHandler): use global instance

--- Game/src/Utils/SceneHandler.cs
+++ Game/src/Utils/SceneHandler.cs
@@ -1,19 +1,21 @@
+using Chickensoft.GoDotNet;
 using Godot;
+using tiktaktoe.Autoload;
 
 namespace tiktaktoe.Utils;
 
 public partial class SceneHandler : Node
 {
 	[Export] public string Scene { get; set; } = "";
-	[Export] private float _fadeOutSpeed = 1.0f;
-	[Export] private float _fadeInSpeed = 1.0f;
+	[Export(PropertyHint.Range, "0,1,")] private float _fadeOutSpeed = 1.0f;
+	[Export(PropertyHint.Range, "0,1,")] private float _fadeInSpeed = 1.0f;
 	[Export] private string _fadeOutPattern = "fade";
 	[Export] private string _fadeInPattern = "fade";
-	[Export] private float _fadeOutSmoothness = 0.1f; // (float, 0, 1)
-	[Export] private float _fadeInSmoothness = 0.1f; // (float, 0, 1)
+	[Export(PropertyHint.Range, "0,1,")] private float _fadeOutSmoothness = 0.1f; // (float, 0, 1)
+	[Export(PropertyHint.Range, "0,1,")] private float _fadeInSmoothness = 0.1f; // (float, 0, 1)
 	[Export] private bool _fadeOutInverted;
 	[Export] private bool _fadeInInverted;
-	[Export] private Color _color = new(0, 0, 0);
+	[Export(PropertyHint.ColorNoAlpha)] private Color _color = new(0, 0, 0);
 	[Export] private float _timeout;
 	[Export] private bool _clickable;
 	[Export] private bool _addToBack = true;
@@ -22,27 +24,27 @@ public partial class SceneHandler : Node
 	private Variant _fadeInOptions;
 	private Variant _generalOptions;
 
-	private Node _sceneManager = null!;
+	private Global Global => this.Autoload<Global>();
+	private Node SceneManager => Global.SceneManager;	
 
 	public override void _Ready()
 	{
-		_sceneManager = GetNode("/root/SceneManager");
 		_fadeOutOptions =
-			_sceneManager.CreateOptions(_fadeOutSpeed, _fadeOutPattern, _fadeOutSmoothness, _fadeOutInverted);
+			SceneManager.CreateOptions(_fadeOutSpeed, _fadeOutPattern, _fadeOutSmoothness, _fadeOutInverted);
 		_fadeInOptions =
-			_sceneManager.CreateOptions(_fadeInSpeed, _fadeInPattern, _fadeInSmoothness, _fadeInInverted);
-		_generalOptions = _sceneManager.CreateGeneralOptions(_color, _timeout, _clickable, _addToBack);
+			SceneManager.CreateOptions(_fadeInSpeed, _fadeInPattern, _fadeInSmoothness, _fadeInInverted);
+		_generalOptions = SceneManager.CreateGeneralOptions(_color, _timeout, _clickable, _addToBack);
 
-		var fadeInFirstSceneOptions = _sceneManager.CreateOptions(1, "fade");
-		var firstSceneGeneralOptions = _sceneManager.CreateGeneralOptions(new Color(0, 0, 0), 1, false);
-		_sceneManager.ShowFirstScene(fadeInFirstSceneOptions, firstSceneGeneralOptions);
+		var fadeInFirstSceneOptions = SceneManager.CreateOptions(1, "fade");
+		var firstSceneGeneralOptions = SceneManager.CreateGeneralOptions(new Color(0, 0, 0), 1, false);
+		SceneManager.ShowFirstScene(fadeInFirstSceneOptions, firstSceneGeneralOptions);
 		// code breaks if scene is not recognizable
-		_sceneManager.ValidateScene(Scene);
+		SceneManager.ValidateScene(Scene);
 		// code breaks if pattern is not recognizable
-		_sceneManager.ValidatePattern(_fadeOutPattern);
-		_sceneManager.ValidatePattern(_fadeInPattern);
+		SceneManager.ValidatePattern(_fadeOutPattern);
+		SceneManager.ValidatePattern(_fadeInPattern);
 	}
 
 	public void OnPressed()
-		=> _sceneManager.ChangeScene(Scene, _fadeOutOptions, _fadeInOptions, _generalOptions);
+		=> SceneManager.ChangeScene(Scene, _fadeOutOptions, _fadeInOptions, _generalOptions);
 }
