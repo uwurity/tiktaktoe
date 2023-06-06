af14c617527827dfc3eb3e02a7d8f51b3c6648f7
Daniel Phan <danielphan.2003@gmail.com>
2023-06-07 04:59:23 +0700

    feat(server): handle match and rewrite fly config
    Signed-off-by: Daniel Phan <danielphan.2003@gmail.com>

--- Server/start.sh
+++ Server/start.sh
@@ -4,9 +4,21 @@ set -eu
 
 sh /nakama/pre_start.sh
 
+sh /ts/start.sh
+
+TS_IP=$(/ts/tailscale ip -4)
+
 echo "Starting to Fly..."
-exec /nakama/nakama --name nakama1 \
+
+cd /nakama/
+mkdir -p data/modules
+mv index.js data/modules
+
+exec /nakama/nakama --name tiktaktoe1 \
   --database.address "$DATABASE_URL" \
   --logger.level DEBUG \
+  --runtime.js_entrypoint "./index.js" \
   --session.token_expiry_sec 7200 \
-  --metrics.prometheus_port 9091
+  --metrics.prometheus_port 9091 \
+  --console.address "$TS_IP" \
+  --console.port 80
